e7e9779939869b17b302accaafb5337549c6f977
Chester <91867272+fullstackchester@users.noreply.github.com>
2022-04-03 11:39:12 +0800

    sample-read from imported json

--- src/components/FunctionPanel.jsx
+++ src/components/FunctionPanel.jsx
@@ -8,27 +8,31 @@ export default function FunctionPanel(props) {
     const headerData = [
         {
             title: 'Add',
-            icon: <IoAdd />
+            icon: <IoAdd />,
+            function: props.add
         },
         {
             title: 'Modify',
-            icon: <IoPencil />
+            icon: <IoPencil />,
+            function: props.modify
         },
         {
             title: 'Delete',
-            icon: <IoRemove />
+            icon: <IoRemove />,
+            function: props.delete
         },
 
     ]
     return (
-        <div className='w-full h-10 bg-white sticky top-14 flex justify-end shadow-sm'>
+        <div className='w-full h-10 bg-zinc-800 rounded-sm sticky top-14 flex justify-end shadow-sm'>
             <div className='h-full w-fit'>
                 <ul className='h-full w-60 flex flex-row justify-evenly items-center text-xs font-medium poppins'>
                     {headerData.map((val, key) => {
                         return (
                             <li
                                 key={key}
-                                className='w-auto h-full px-4 flex flex-row items-center cursor-pointer hover:bg-zinc-200'>
+                                onClick={val.function}
+                                className='w-auto h-full px-4 flex flex-row text-white font-light items-center cursor-pointer hover:bg-zinc-700'>
                                 {val.title}
                                 <span className='ml-1'> {val.icon} </span>
 
--- src/pages/Subjects.jsx
+++ src/pages/Subjects.jsx
@@ -1,25 +1,48 @@
-import React from 'react'
+import React, { useState } from 'react'
 import FunctionPanel from '../components/FunctionPanel'
+import { DummyData } from '../Data/DummyData'
 
 export default function Subjects() {
+
+
+    const [selected, setSelected] = useState([])
+    const addSub = () => alert('you tried to add a subject')
+
+    function modifySub() {
+        console.log(selected)
+    }
+
     return (
         <div className='w-full h-auto  flex flex-col'>
-            <FunctionPanel />
-            <div className='w-full h-[auto] flex flex-row flex-wrap justify-start content-start py-2'>
-                <div className='w-[24%] h-60 rounded-md bg-white mr-2 mb-2'></div>
-                <div className='w-[24%] h-60 rounded-md bg-white mr-2 mb-2'></div>
-                <div className='w-[24%] h-60 rounded-md bg-white mr-2 mb-2'></div>
-                <div className='w-[24%] h-60 rounded-md bg-white mr-2 mb-2'></div>
-                <div className='w-[24%] h-60 rounded-md bg-white mr-2 mb-2'></div>
-                <div className='w-[24%] h-60 rounded-md bg-white mr-2 mb-2'></div>
-                <div className='w-[24%] h-60 rounded-md bg-white mr-2 mb-2'></div>
-                <div className='w-[24%] h-60 rounded-md bg-white mr-2 mb-2'></div>
-                <div className='w-[24%] h-60 rounded-md bg-white mr-2 mb-2'></div>
-                <div className='w-[24%] h-60 rounded-md bg-white mr-2 mb-2'></div>
-                <div className='w-[24%] h-60 rounded-md bg-white mr-2 mb-2'></div>
-                <div className='w-[24%] h-60 rounded-md bg-white mr-2 mb-2'></div>
-                <div className='w-[24%] h-60 rounded-md bg-white mr-2 mb-2'></div>
-                <div className='w-[24%] h-60 rounded-md bg-white mr-2 mb-2'></div>
+            <FunctionPanel
+                add={addSub}
+                modify={modifySub}
+            />
+            <div className='w-full h-[auto] flex flex-col flex-wrap justify-start content-start py-2'>
+                {DummyData.map((val, key) => {
+                    return (
+                        <div
+                            key={key}
+                            className='w-full h-16 bg-white border-b border-slate-200 justify-between items-center flex flex-row p-2 poppins'>
+                            <div className='flex flex-col'>
+                                <h4 className='text-lg   font-medium'>{val.name.toUpperCase()} </h4>
+                                <span className='text-xs font-medium text-zinc-600 whitespace-pre-wrap'>{val.email} </span>
+                            </div>
+                            <input
+                                type='checkbox'
+                                onChange={(e) => {
+                                    setSelected([
+                                        ...selected, {
+                                            name: val.name,
+                                            email: val.email
+                                        }
+                                    ])
+                                }} />
+
+                        </div>
+                    )
+                })}
+
             </div>
 
         </div>
